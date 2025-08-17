16e90b16e6a806ace8477121ae9112566b8d6a01
Matheus Reinheimer <mreinheimer0@gmail.com>
2025-08-16 23:13:49 -0300

    feat: add end-to-end tests for account creation and authentication

--- src/controllers/authenticate.controller.e2e.spec.ts
+++ src/controllers/authenticate.controller.e2e.spec.ts
@@ -1,5 +1,6 @@
 import type { INestApplication } from '@nestjs/common'
 import { Test } from '@nestjs/testing'
+import { hash } from 'argon2'
 import request from 'supertest'
 import { AppModule } from '@/app.module'
 import { PrismaService } from '@/prisma/prisma.service'
@@ -20,21 +21,23 @@ describe('Create account (E2E)', () => {
     await app.init()
   })
 
-  test('[POST] /accounts', async () => {
-    const response = await request(app.getHttpServer()).post('/accounts').send({
-      name: 'John Doe',
+  test('[POST] /sessions', async () => {
+    await prisma.user.create({
+      data: {
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: await hash('123456'),
+      },
+    })
+
+    const response = await request(app.getHttpServer()).post('/sessions').send({
       email: 'johndoe@example.com',
       password: '123456',
     })
 
     expect(response.statusCode).toBe(201)
-
-    const userOnDataBase = await prisma.user.findUnique({
-      where: {
-        email: 'johndoe@example.com',
-      },
+    expect(response.body).toEqual({
+      accessToken: expect.any(String),
     })
-
-    expect(userOnDataBase).toBeTruthy()
   })
 })
